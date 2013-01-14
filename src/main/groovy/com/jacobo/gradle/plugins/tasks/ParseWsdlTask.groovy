80a910ef0a3931cc3053d7e8e157cfe750f62fe6
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-01-13 21:38:03 -0500

    editring the parse wsdl task to exclude the input file, for some reason, it doesn't work if it is dynamic, have to figure out a better solution, did change the destionation Output Directory, and it works fine.

--- src/main/groovy/com/jacobo/gradle/plugins/tasks/ParseWsdlTask.groovy
+++ src/main/groovy/com/jacobo/gradle/plugins/tasks/ParseWsdlTask.groovy
@@ -1,6 +1,7 @@
 package com.jacobo.gradle.plugins.tasks
 
 import com.jacobo.gradle.plugins.WsdlExtension
+import com.jacobo.gradle.plugins.WsdlPlugin
 
 import org.gradle.api.logging.Logging
 import org.gradle.api.logging.Logger
@@ -18,28 +19,30 @@ class ParseWsdlTask extends DefaultTask {
   static final Logger log = Logging.getLogger(ParseWsdlTask.class)
 
   final WsdlExtension extension = project.extensions.wsdl
-
-  @InputFile
-  File wsdlPath = extension.wsdlPath
   
   @OutputDirectory
-  String destinationDirectory = extension.sourceDestinationDirectory
+  File destinationDirectory
 
   @TaskAction
   void parseWsdl() { 
+    log.info("parsing wsdl")
+    log.info("wsdl Path is {}", project.extensions.wsdl.wsdlPath)
+    log.info("wsdl path is {}", project.wsdl.wsdlPath)
+    log.info("destination directory is {}", destinationDirectory)
+    
     ant.taskdef (name : 'wsimport', classname: 'com.sun.tools.ws.ant.WsImport', classpath: project.configurations[WsdlPlugin.WSDL_CONFIGURATION_NAME].asPath)
 
     ant.wsimport (
-    wsdl: wsdlPath.absolutePath,
+    wsdl: extension.wsdlPath.path,
     verbose: extension.verbose,
-    sourcedestdir : destinationDirectory,
+    sourcedestdir : destinationDirectory.path,
     keep: extension.keep,
     wsdlLocation : extension.wsdlLocation,
     xnocompile : extension.xnocompile,
     fork : extension.fork,
     xdebug : extension.xdebug,
     target : extension.target) {
-      episode.each { episode ->
+      extension.episodes.each { episode ->
 	binding(dir : extension.episodeDirectory.path, includes : "${episode}.episode")
       }
     }
