56111a5d6bd3e0d7c41764f04d5019b885276cd7
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-01-13 21:41:09 -0500

    adding an examples folder, so you can see the plugin in action

---
+++ examples/hello-world-ws/src/main/java/helloworld/sample/ibm/com/ObjectFactory.java
@@ -0,0 +1,48 @@
+
+package helloworld.sample.ibm.com;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the helloworld.sample.ibm.com package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: helloworld.sample.ibm.com
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Hello }
+     * 
+     */
+    public Hello createHello() {
+        return new Hello();
+    }
+
+    /**
+     * Create an instance of {@link HelloResponse }
+     * 
+     */
+    public HelloResponse createHelloResponse() {
+        return new HelloResponse();
+    }
+
+}
---
+++ examples/hello-world-ws/src/main/java/helloworld/sample/ibm/com/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:helloWorld/sample/ibm/com")
+package helloworld.sample.ibm.com;
