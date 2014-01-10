7b8a067d926bd844d2431ac99624c5ecaa2b071f
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2014-01-10 14:26:42 -0500

    adding a comment to the base spec class

--- src/test/groovy/com/jacobo/gradle/plugins/BaseSpecification.groovy
+++ src/test/groovy/com/jacobo/gradle/plugins/BaseSpecification.groovy
@@ -2,8 +2,13 @@ package com.jacobo.gradle.plugins
 
 import spock.lang.Specification
 
+/**
+ * utility methods that most every test uses.
+ */
 class BaseSpecification extends Specification {
+
   def getFileFromResourcePath(path) {
     return new File(this.getClass().getResource(path).toURI())
   }
+
 }
