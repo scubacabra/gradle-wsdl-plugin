04fec28ee80d24330a1143066a1a44c2ba6227d2
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2014-01-10 16:31:08 -0500

    rename of the instance variables to make their meaning and usage clearer

--- src/main/groovy/com/jacobo/gradle/plugins/tasks/War.groovy
+++ src/main/groovy/com/jacobo/gradle/plugins/tasks/War.groovy
@@ -22,31 +22,51 @@ class War extends War {
 
     static final Logger log = Logging.getLogger(War.class)
 
+    /**
+     * The name of the folder in the war that all wsdl Files will go into
+     */
     @Input
     String wsdlFolder
 
+    /**
+     * The name of the folder in the war that all schema (xsd) files will go into
+     */
     @Input
     String schemaFolder
 
+    /**
+     * The actual directory in the build/$webServicesOutputDir/$wsdlDirectory
+     * where all the wsdl files are that this project depends on
+     */
     @InputDirectory
-    File wsdl
+    File wsdlDirectory
 
+    /**
+     * The actual directory in the build/$webServicesOutputDir/xsdDirectory
+     * where all the xsd files are that this project depends on
+     */
     @InputDirectory
-    File schema
+    File schemaDirectory
 
+    /**
+     * I had to do this in the constructor, it was impossible for me, at least
+     * when I attemped this, to call super, and then have another method
+     * do the into and from.  That was how I did all of the other tasks, but
+     * alas, not this one.
+     */
     War() {
         super()
         log.debug("Calling war constructor")
 
         webInf.into({ getWsdlFolder() }) {
             from {
-                getWsdl()
+                getWsdlDirectory()
             }
         }
 
         webInf.into({ getSchemaFolder() }) {
             from {
-                getSchema()
+                getSchemaDirectory()
             }
         }
     }
