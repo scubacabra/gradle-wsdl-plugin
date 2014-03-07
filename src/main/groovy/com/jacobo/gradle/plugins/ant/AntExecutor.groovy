d8ec24cbc443a9161c9e2bfbf2bb77fb42443078
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2014-03-07 15:07:35 -0500

    AntExecutor interface needed a change.  Passing in an AntBuilder to build and run as well as a map of arguments that is used to configure the AntBuilder.  AntWsImport is changed to reflect this interface change in its service

--- src/main/groovy/com/jacobo/gradle/plugins/ant/AntExecutor.groovy
+++ src/main/groovy/com/jacobo/gradle/plugins/ant/AntExecutor.groovy
@@ -1,9 +1,13 @@
 package com.jacobo.gradle.plugins.ant
 
+import groovy.util.AntBuilder
+
 interface AntExecutor {
   
   /**
    * Executes the ant task the implementation is executing.
+   * @param ant AntBuilder to build and execute
+   * @param arguments to configure antBuilder with
    */
-  public void execute()
+  public void execute(AntBuilder ant, Map<String, Object> arguments)
 }
--- src/main/groovy/com/jacobo/gradle/plugins/ant/AntWsImport.groovy
+++ src/main/groovy/com/jacobo/gradle/plugins/ant/AntWsImport.groovy
@@ -1,86 +1,45 @@
 package com.jacobo.gradle.plugins.ant
 
+import groovy.util.AntBuilder
+
 import com.jacobo.gradle.plugins.extension.WsImportExtension
 
 class AntWsImport implements AntExecutor {
 
   /**
-   * wsdl file reference
+   * run the ant wsimport task an #wsdl
+   * @param ant the ant builder to build and execute
+   * @param arguments a map of arguments to use in the ant file
+   *   "wsdl"=> the file to parse with wsimport
+   *   "extension" => WsImportExtension wsdl plugins wsimport extension for configuration
+   *   "classpath" => configuration path as String classpath for the ant task def as defined by the wsdl-configuration.asPath
    */
-  File wsdl
-  
-  /**
-   * used for ant.taskdef classpath
-   */
-  def configurationPath
+  public void execute(AntBuilder ant, Map<String, Object> arguments) {
+    def wsdl = arguments["wsdl"]
+    def extension = arguments["extension"]
+    def classpath = arguments["classpath"]
 
-  /**
-   * ant wsimport parameter -- where to place generated source
-   * see #https://jax-ws.java.net/${jaxws-version}/docs/wsimportant.html#
-   */
-  File destinationDirectory
-
-  /**
-   * ant wsimport parameter flags
-   * see #https://jax-ws.java.net/${jaxws-version}/docs/wsimportant.html#
-   */
-  boolean verbose, keep, xnocompile, fork, xdebug
-
-  /**
-   * ant wsimport parameters
-   * see #https://jax-ws.java.net/${jaxws-version}/docs/wsimportant.html#
-   */
-  String target, wsdlLocation
-
-  /**
-   * Directory where to find episode files
-   */
-  File epsiodeDirectory
-
-  /**
-   * list of specific episode file NAMES ONLY, to get from episode directory
-   */
-  List episodes
-
-  /**
-   * construct this ant service with the WsImportExtension
-   */
-  AntWsImport(File wsdlFile, configurationPath, WsImportExtension wsimportExtension) {
-    wsdl = wsdlFile
-    this.configurationPath = configurationPath
-    destinationDirectory = wsimportExtension.sourceDestinationDirectory
-    verbose = wsimportExtension.verbose
-    keep = wsimportExtension.keep
-    xnocompile = wsimportExtension.xnocompile
-    fork = wsimportExtension.fork
-    xdebug = wsimportExtension.xdebug
-    target = wsimportExtension.target
-    wsdlLocation = wsimportExtension.wsdlLocation
-    episodeDirectory = wsimportExtension.episodeDirectory
-    episodes = wsimportExtension.episodes
-  }
-
-  public void execute() {
     log.info("parsing wsdl '{}' with destination directory of '{}'",
-	     wsdl, destinationDirectory)
+	     wsdl, extension.destinationDirectory)
     
     ant.taskdef (name : 'wsimport',
 		 classname: 'com.sun.tools.ws.ant.WsImport',
-		 classpath: configurationPath)
+		 classpath: classpath)
 
     ant.wsimport ( wsdl            : wsdl.path,
-		   verbose         : verbose,
-		   sourcedestdir   : destinationDirectory.path,
-		   keep            : keep,
-		   wsdlLocation    : wsdlLocation,
-		   xnocompile      : xnocompile,
-		   fork            : fork,
-		   xdebug          : xdebug,
-		   target          : target
-		 ) { episodes.each { episode ->
-		   log.debug("binding  '{}.episode' at location '{}'", episode, episodeDirectory.path)
-		   binding(dir : episodeDirectory.path, includes : "${episode}.episode")
-      }
+		   verbose         : extension.verbose,
+		   sourcedestdir   : extenstion.destinationDirectory.path,
+		   keep            : extension.keep,
+		   wsdlLocation    : extension.wsdlLocation,
+		   xnocompile      : extension.xnocompile,
+		   fork            : extension.fork,
+		   xdebug          : extension.xdebug,
+		   target          : extension.target
+		 ) { extension.episodes.each { episode ->
+		   log.debug("binding  '{}.episode' at location '{}'", episode,
+			     extension.episodeDirectory.path)
+		   binding(dir : extension.episodeDirectory.path,
+			   includes : "${episode}.episode") }
     }
   }
 }
