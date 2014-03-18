7dc90e68cbea2ed842b3472d601fece70dc2320b
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2014-03-18 13:16:54 -0400

    new convert project name task specification

---
+++ src/test/groovy/com/jacobo/gradle/plugins/tasks/ConvertProjNameToWsdlSpec.groovy
@@ -0,0 +1,31 @@
+package com.jacobo.gradle.plugins.tasks
+
+import com.jacobo.gradle.plugins.ProjectTaskSpecification
+
+import com.jacobo.gradle.plugins.WsdlPlugin
+import com.jacobo.gradle.plugins.convert.NameToFileConverter
+
+class ConvertProjNameToWsdlSpec extends ProjectTaskSpecification {
+
+  def nameConverter = Mock(NameToFileConverter)
+  def wsdlFile = new File("wsdlDir/some-wsdl.wsdl")
+
+  def setup() {
+    buildProject("wsdl")
+    task = project.tasks[WsdlPlugin.CONVERSION_TASK_NAME] as ConvertProjNameToWsdl
+    task.with {
+      converter = nameConverter
+      wsdlDirectory = new File("wsdlDir")
+      projectName = project.name
+    }
+  }
+
+  def "convert project name #project.name to wsdl File #wsdlFile"() {
+    when:
+    task.start()
+
+    then:
+    1 * nameConverter.convert(project.name, new File("wsdlDir")) >> wsdlFile
+    project.wsdl.wsdlFile == wsdlFile
+  }
+}
